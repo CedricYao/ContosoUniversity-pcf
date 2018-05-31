3a3e502ed6405f44ef661ed83ec10add8eacd1ae
Cedric Yao <raspar@gmail.com>
2018-05-30 21:28:26 -0500

    added nuget package provider to configuration script

--- configure-build.ps1
+++ configure-build.ps1
@@ -1,8 +1,16 @@
 # pull dependencies
+
+$has_nuget = Get-PackageProvider -ListAvailable | Select-String -Pattern "NuGet" -Quiet
+if(-Not($has_nuget)) {
+	#install Nuget Package Provider
+	Write-Host "No Nuget Package Provider Found: Installing now" -ForegroundColor Red
+	Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+}
+
 $has_vsSetup = Get-Module -ListAvailable | Select-String -Pattern "VSSetup" -Quiet
 if(-Not($has_vsSetup)) {
 	#install VSSetup
-	Write-Host "Installing VSSetup" -ForegroundColor Yellow
+	Write-Host "No VSSetup Module Found: Installing now" -ForegroundColor Red
 	Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 	Install-Module VSSetup -Scope CurrentUser
 }
@@ -10,7 +18,7 @@ if(-Not($has_vsSetup)) {
 $has_psake = Get-Module -ListAvailable | Select-String -Pattern "Psake" -Quiet
 if(-Not($has_psake)) {
 	#install psake
-	Write-Host "Installing Psake" -ForegroundColor Yellow
+	Write-Host "No Psake Module Found: Installing now" -ForegroundColor Red
 	Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 	Install-Module Psake -Scope CurrentUser
 }
