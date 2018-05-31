29b182c6e6dd94d3409061862e154fe33a136e48
Cedric Yao <cyao@pivotal.io>
2018-05-31 10:05:11 -0500

    removed test for syntax error

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo Executing build with build.ps1 configuration
 @echo off
 powershell.exe -NoProfile -ExecutionPolicy bypass -Command "& {.\configure-build.ps1 }"
-powershell.exe -NoProfile -ExecutionPolicy bypass -Command "& {invoke-psake .\build.ps1 %1 -parameters @{"version"="'%2'"}; if ($lastexitcode -ne 0) {write-host "ERROR: $lastexitcode" -fore RED; exit $lastexitcode} }"
+powershell.exe -NoProfile -ExecutionPolicy bypass -Command "& {invoke-psake .\build.ps1 %1 -parameters @{"version"="'%2'"};  exit !($psake.build_success) }"
